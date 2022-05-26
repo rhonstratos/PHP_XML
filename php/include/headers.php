423ba0d39dc58704029d09c80c1a657d8bac0842
rhonstratos <kizunaai20@gmail.com>
2022-05-26 16:08:25 +0800

    webpack

--- php/include/headers.php
+++ php/include/headers.php
@@ -1,10 +1,8 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- CSS only -->
-<link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- JavaScript Bundle with Popper -->
-<script src="../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js" defer></script>
+<!-- webpack5 compile-->
+<script src="../dist/main.js"></script>
 <!-- Local JavaScript and CSS -->
 <script src="../js/main.js" defer></script>
 <link rel="stylesheet" href="../css/main.css">
