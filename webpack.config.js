99e5e2adf1ce50f4dcdb9add92b4c98a933d1bee
rhonstratos <kizunaai20@gmail.com>
2022-06-07 09:03:09 +0800

    removed svg from webpack rule

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
                 }]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(png|jpg|gif)$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
