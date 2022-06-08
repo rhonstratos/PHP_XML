ec75065c785031b1c9802e3c4c6561d7f3571a6e
rhonstratos <kizunaai20@gmail.com>
2022-06-08 11:00:49 +0800

    rename card

--- php/include/config.php
+++ php/include/config.php
@@ -31,7 +31,7 @@ class Index extends XMLHandler
                 str_contains(strtolower($modelNumber), strtolower($searchValue))
             ) {
                 $flag += 1;
-                require('cards/indexCard.php');
+                require('cards/index.php');
             }
         }
         switch ($flag) {
@@ -54,7 +54,7 @@ class Index extends XMLHandler
             $modelNumber = $targetNode->getelementsByTagName("modelNumber")[0]->nodeValue;
             $variantName = $targetNode->getelementsByTagName("variantName")[0]->nodeValue;
             $img = $targetNode->getelementsByTagName("img")[0]->nodeValue;
-            require('cards/indexCard.php');
+            require('cards/index.php');
         }
     }
 }
