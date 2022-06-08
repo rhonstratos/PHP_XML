c842df85d33fc6064f186c0d79caff18fb8205d7
rhonstratos <kizunaai20@gmail.com>
2022-06-08 15:54:36 +0800

    prune js

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,11 @@
 //import 'bootstrap/dist/js/bootstrap.esm.min'
 import '../scss/app'
 import { newMemory, newProcessor, newStorage, replaceIMG } from './register'
-import { callToast } from "./main";
 import { viewCard, register, viewEdit, update, viewDelete, deleteNode } from './ajax.class'
 
 window.newCPU = newProcessor
 window.newMemory = newMemory
 window.newStorage = newStorage
-window.callToast = callToast
 window.viewCard = viewCard
 window.viewEdit = viewEdit
 window.viewDelete = viewDelete
